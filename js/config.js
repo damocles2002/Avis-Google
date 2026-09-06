13cc1533f107b86eed4b5c78cb4b05aacf593708
damocles2002 <damocles2002@users.noreply.github.com>
2026-09-06 20:27:14 +0000

    Jukebox: 2 modes (auto avec recherche musicale / playlist) + config cle API YouTube

--- js/config.js
+++ js/config.js
@@ -7,4 +7,5 @@ const CONFIG = {
     SB_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycGZnaWFvY3hmbnlmZG9pYXBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NDQ3MzMsImV4cCI6MjA5MDUyMDczM30.8OkTr_1pXbPn73e64Vgg8ovLGnkNNGDdWvmrzVBmzwo",
     APP_NAME: "DAMAVIS",
     ADMIN_EMAIL: "ngongangdjomo@gmail.com",
+    YOUTUBE_API_KEY: "", // clé API YouTube pour la recherche musicale (à remplir)
 };
